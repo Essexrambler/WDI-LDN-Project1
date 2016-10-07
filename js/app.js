76ed9b48dcbd3faa033da5947c8825bd0c59f083
reynoldsluke08@gmail.com <reynoldsluke08@gmail.com>
2016-10-07 10:22:24 +0100

    new work copied over

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log('JS loaded');
+// });
+//
+// var clicks = 0; //Counts how many cards pciked in turn
+// var firstChoice; //stores index of first card
+// var secondChoice ; //stroes index of second card
+//
+// var match = 0; //counts matches to add to click counter
+// var backCard = "image.png";
+
+var cards = document.querySelectorAll'li');
+console.log(tiles);
