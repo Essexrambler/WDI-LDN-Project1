e59a2103762affb3592815ab4539f140ca87e84a
reynoldsluke08@gmail.com <reynoldsluke08@gmail.com>
2016-10-11 15:14:02 +0100

    Almost finished logic

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
   for(var i=0;i<cards.length;i++) {
     cards[i].addEventListener('click', function() {
 
-    var selection = this.getAttribute("data-image");
-    moves.push(selection);
-    clickedElements.push(this);
+      var selection = this.getAttribute("data-image");
+      moves.push(selection);
+      clickedElements.push(this);
 
-    console.log(moves);
-    console.log(clickedElements);
+      console.log(moves);
+      console.log(clickedElements);
 
 
       var image;
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
   function checkWin () {
     if (moves.length == 2) {
       if (moves[0] == moves[1]) {
-        alert("match");
+
         moves = [];
         clickedElements = [];
       } else {
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
           clickedElements[1].style.backgroundImage = "";
           moves = [];
           clickedElements = [];
-        }, 500);
+        }, 700);
 
 
 
@@ -91,81 +91,94 @@ document.addEventListener("DOMContentLoaded", function() {
   var cardList = [
     {
       name: 'card_1',
+      image: 'bigredbus',
       id: 1
     },
     {
       name: 'card_2',
+      image: 'bridgeonfire',
       id: 2
     },
     {
       name: 'card_3',
+      image:'switchhouse',
       id: 3
     },
     {
       name: 'card_4',
+      image:'stpauls',
       id: 4
     },
     {
       name: 'card_5',
+      image:'shard',
       id: 5
     },
     {
       name: 'card6',
+      image:'blackfriarsbridge',
       id: 6
     },
     {
       name: 'card_7',
+      image:'cityhall',
       id: 7
     },
     {
       name: 'card_8',
+      image:'sunsetshard',
       id: 8
     },
     {
       name: 'card_9',
+      image:'bigredbus',
       id: 9
     },
     {
       name: 'card_10',
+      image:'bridgeonfire',
       id: 10
     },
     {
       name: 'card_11',
+      image:'switchhouse',
       id: 11
     },
     {
       name: 'card_12',
+      image:'stpauls',
       id: 12
     },
     {
       name: 'card_13',
+      image:'shard',
       id: 13
     },
     {
       name: 'card_14',
+      image:'blackfriarsbridge',
       id: 14
     },
     {
       name: 'card_15',
+      image:'cityhall',
       id: 15
     },
     {
       name: 'card_16',
+      image:'sunsetshard',
       id: 16
     },
-  ],
+  ];
 
-  card = [""];
-  cardId = ["#"];
-  cardsFlipped = 0;
-  appendToBoard = document.getElementById('cardTable');
+  var appendToBoard = document.getElementById('cardTable');
 
   function shuffle(array) {
     var currentIndex = array.length,
     temporaryValue,
     randomIndex;
 
-    while (0 !== currentIndex) {
+    while (currentIndex !== 0) {
 
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
@@ -175,40 +188,27 @@ document.addEventListener("DOMContentLoaded", function() {
       array[randomIndex] = temporaryValue;
 
     }
-    return  array;
+    cardList = array;
+    return array;
   }
 
   var gameBoard = document.getElementById('li');
 
-  function createCardDeck() {
-    shuffle(cardlist);
-    var card= '';
-    for(var i = 0 ; i< cardList.length; i++){
-      card= '<span class="card" id="' + cardList[i].id +'">' +
-      '<img class="card_back" alt="Union jack card back"/>' +
-      '<img class="card_front inner-img" src="images/' + cardList[i].name + '.jpg" />' +
-      '</span>';
-    }
-  }
+  function insertShuffledCardDeck() {
+    var cardHTML = '';
 
-  appendToBoard.innerHTML = card;
+    shuffle(cardList);
+    for(var i = 0 ; i< cardList.length; i++){
+      // <li id="card1" class="card" data-image="bigredbus"></li>
+      cardHTML += '<li id="' + cardList[i].name + '" class="card" data-image="' + cardList[i].image + '"></li>';
+    }
+    appendToBoard.innerHTML = cardHTML;
+  }
 
   document.getElementById('btnReset').addEventListener('click', function(e) {
-    event.preventDefault();
-    createCardDeck();
+    event.preventDefault(); //calling the shuffle image-list
+    insertShuffledCardDeck();{}
+
+    return gameBoard;
   });
-
-  function flipCard() {
-    var card = document.querySelectorAll('.card');
-
-    for ( var i=0; i < card.length; i++) {
-      card[i].addEventListener('mousedown', function(e) {
-        event.currentTarget.classList.toggle('flipped');
-        return false;
-        // function shuffle (array) {}
-      });
-    }
-
-    createCardDeck();
-  }
 });
