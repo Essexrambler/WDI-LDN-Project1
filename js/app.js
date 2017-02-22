2d0ffd1d2bcd50baa4966960e463f4504703e783
reynoldsluke08@gmail.com <reynoldsluke08@gmail.com>
2017-02-22 15:50:34 +0000

    optimized images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   // Console log
   console.log('Document loaded');
 
   var playerScore = 0;
-  var cards = document.querySelectorAll(".card");
+  var cards = document.querySelectorAll('.card');
   var moves = [];
   var clickedElements = [];
   var appendToBoard = document.getElementById('cardTable');
@@ -11,84 +11,83 @@ document.addEventListener("DOMContentLoaded", function() {
   var cardList = [
     {name: 'card1',image: 'bigredbus',id: 1},
     {name: 'card2',image: 'bridgeonfire',id: 2},
-    {name: 'card3',image:'switchhouse',id: 3},
-    {name: 'card4',image:'stpauls',id: 4},
-    {name: 'card5',image:'shard',id: 5},
-    {name: 'card6',image:'blackfriarsbridge',id: 6},
-    {name: 'card7',image:'cityhall',id: 7},
-    {name: 'card8',image:'sunsetshard',id: 8},
-    {name: 'card9',image:'bigredbus',id: 9},
-    {name: 'card10',image:'bridgeonfire',id: 10},
-    {name: 'card11',image:'switchhouse',id: 11},
-    {name: 'card12',image:'stpauls',id: 12},
-    {name: 'card13',image:'shard',id: 13},
-    {name: 'card14',image:'blackfriarsbridge',id: 14},
-    {name: 'card15',image:'cityhall',id: 15},
-    {name: 'card16',image:'sunsetshard',id: 16},
+    {name: 'card3',image: 'switchhouse',id: 3},
+    {name: 'card4',image: 'stpauls',id: 4},
+    {name: 'card5',image: 'shard',id: 5},
+    {name: 'card6',image: 'blackfriarsbridge',id: 6},
+    {name: 'card7',image: 'cityhall',id: 7},
+    {name: 'card8',image: 'sunsetshard',id: 8},
+    {name: 'card9',image: 'bigredbus',id: 9},
+    {name: 'card10',image: 'bridgeonfire',id: 10},
+    {name: 'card11',image: 'switchhouse',id: 11},
+    {name: 'card12',image: 'stpauls',id: 12},
+    {name: 'card13',image: 'shard',id: 13},
+    {name: 'card14',image: 'blackfriarsbridge',id: 14},
+    {name: 'card15',image: 'cityhall',id: 15},
+    {name: 'card16',image: 'sunsetshard',id: 16}
   ];
 
-function gamePlay(){
-  for(var i=0;i<cards.length;i++) {
-    cards[i].addEventListener('click', function() {         //Gameplay function created too call the data
-      var selection = this.getAttribute("data-image");      //in one function, contains all the images
-      moves.push(selection);                                //the win function and style background.
-      clickedElements.push(this);
-      console.log(moves);
-      console.log(clickedElements);
-      var image;
-      switch(this.id) {                                     //managed to refract the image switch by coupling
-        case 'card1':                                      //the pairs with equal images.
-        case 'card9':
-        image = "url('images/Archive/bigredbus.jpg')";
-        break;
+  function gamePlay(){
+    for(var i=0;i<cards.length;i++) {
+      cards[i].addEventListener('click', function() {         //Gameplay function created too call the data
+        var selection = this.getAttribute('data-image');      //in one function, contains all the images
+        moves.push(selection);                                //the win function and style background.
+        clickedElements.push(this);
+        console.log(moves);
+        console.log(clickedElements);
+        var image;
+        switch(this.id) {                                     //managed to refract the image switch by coupling
+          case 'card1':                                     //the pairs with equal images.
+          case 'card9':
+            image = "url('images/Archive/bigredbus.jpg')";
+            break;
 
-        case 'card2':
-        case 'card10':
-        image = 'url(images/Archive/bridgeonfire.jpg)';
-        break;
+          case 'card2':
+          case 'card10':
+            image = 'url(images/Archive/bridgeonfire.jpg)';
+            break;
 
-        case 'card3':
-        case 'card11':
-        image = 'url(images/Archive/switchhouse.jpg)';
-        break;
+          case 'card3':
+          case 'card11':
+            image = 'url(images/Archive/switchhouse.jpg)';
+            break;
 
-        case 'card4':
-        case 'card12':
-        image = 'url(images/Archive/stpauls.jpg)';
-        break;
+          case 'card4':
+          case 'card12':
+            image = 'url(images/Archive/stpauls.jpg)';
+            break;
 
-        case 'card5':
-        case 'card13':
-        image = 'url(images/Archive/shard.jpg)';
-        break;
+          case 'card5':
+          case 'card13':
+            image = 'url(images/Archive/shard.jpg)';
+            break;
 
-        case 'card6':
-        case 'card14':
-        image = 'url(images/Archive/blackfriarsbridge.jpg)';
-        break;
+          case 'card6':
+          case 'card14':
+            image = 'url(images/Archive/blackfriarsbridge.jpg)';
+            break;
 
-        case 'card7':
-        case 'card15':
-        image = 'url(images/Archive/cityhall.jpg)';
-        break;
+          case 'card7':
+          case 'card15':
+            image = 'url(images/Archive/cityhall.jpg)';
+            break;
 
-        case 'card8':
-        case 'card16':
-        image = 'url(images/Archive/sunsetshard.jpg)';
-        break;
-      }
+          case 'card8':
+          case 'card16':
+            image = 'url(images/Archive/sunsetshard.jpg)';
+            break;
+        }
 
-      this.style.backgroundImage = image;
+        this.style.backgroundImage = image;
 
-      checkWin();
-      clicks += 1;
-      document.getElementById("clicks").innerHTML = "Player Moves = " + clicks;
-    });
-
+        checkWin();
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = "Player Moves = " + clicks;
+      });
+    }
   }
-}
 
-function checkWin () {                                 //Win function, checking if moves is equal to
+  function checkWin () {                                 //Win function, checking if moves is equal to
     if (moves.length == 2) {                           //moves length and if not a match 700th of a second
       if (moves[0] == moves[1]) {                      //flip back to backgroundImage.
 
@@ -96,8 +95,8 @@ function checkWin () {                                 //Win function, checking 
         clickedElements = [];
       } else {
         setTimeout(function(){
-          clickedElements[0].style.backgroundImage = "";
-          clickedElements[1].style.backgroundImage = "";
+          clickedElements[0].style.backgroundImage = '';
+          clickedElements[1].style.backgroundImage = '';
           moves = [];
           clickedElements = [];
         }, 700);
@@ -111,8 +110,8 @@ function checkWin () {                                 //Win function, checking 
 
   function shuffle(array) {
     var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+      temporaryValue,
+      randomIndex;
 
     while (currentIndex !== 0) {
 
@@ -147,10 +146,10 @@ function checkWin () {                                 //Win function, checking 
   document.getElementById('btnReset').addEventListener('click', function(e) {
     // event.preventDefault(); //calling the shuffle image-list
     clicks = 0;
-    document.getElementById("clicks").innerHTML = "Player Moves = " + clicks;
+    document.getElementById('clicks').innerHTML = 'Player Moves = ' + clicks;
 
     insertShuffledCardDeck();
-    cards = document.querySelectorAll(".card");
+    cards = document.querySelectorAll('.card');
 
     gamePlay();
 
